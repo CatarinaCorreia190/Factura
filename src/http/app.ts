d95075095cf3a844e39a21b76c1feb6b74fcb957
fanuel.ramos <fanuel.ramos@alien-group.com>
2024-06-02 19:06:53 +0100

    fix: add routes to the api

--- src/http/app.ts
+++ src/http/app.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 import produtoRoutes from "./produtoRoutes";
+import clienteRoutes from "./clienteRoutes";
+import faturaRoutes from "./faturaRoutes";
+import pagamentoRoutes from "./pagamentoRoutes";
 
 const app = express();
 app.use(express.json());
 
 app.use('/api/produto', produtoRoutes);
-app.use('/api/cliente', );
+app.use('/api/cliente', clienteRoutes);
+app.use('/api/fatura', faturaRoutes);
+app.use('/api/pagamento', pagamentoRoutes);
 
 export default app;
